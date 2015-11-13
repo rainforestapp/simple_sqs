a9ff185ab27bceb6d3b5f0d49b285eb3f834dd92
Nicolas Valcarcel <nvalcarcel@gmail.com>
2015-11-13 16:21:42 -0500

    add approximate number of messages

--- spec/simple_sqs/queue_spec.rb
+++ spec/simple_sqs/queue_spec.rb
@@ -23,4 +23,11 @@ describe SimpleSqs::Queue do
       subject.send_message(event_name: event_name, arguments: arguments)
     end
   end
+
+  describe '#approximate_number_of_messages' do
+    it 'gets the number of messages' do
+      expect(subject.client).to receive(:get_queue_atttributes).with(queue_url: ENV.fetch('SIMPLE_SQS_QUEUE_URL'), attribute_names: ['ApproximateNumberOfMessages'])
+      subject.approximate_number_of_messages
+    end
+  end
 end
